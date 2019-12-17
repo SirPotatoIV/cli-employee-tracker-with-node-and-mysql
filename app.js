6d86e00c8af356d90be28c5f5d233f7ff0e2780e
SirPotatoIV <otoolej33@gmail.com>
2019-12-16 22:13:38 -0600

    Added code to add test data to database. Added comments to explain code in seed.sql. Added code to connect to server and read data from sever.

--- app.js
+++ app.js
@@ -3,18 +3,63 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
 
-const testArray = [
-    {name: 'foo',
-    type: 'test'
-}, 
-    {name: 'bar',
-    type: 'test'
-},
-    {name: 'jake',
-    type: 'test'
+// Create connection with server
+function contactServer(){
+    const connection = mysql.createConnection({
+        host: "localhost",
+        
+        // Your port; if not 3306
+        port: 3306,
+        
+        // Your username
+        user: "root",
+        
+        // Your password
+        password: "root",
+        database: "employee_tracker_db"
+    });
+
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log("connected as id " + connection.threadId);
+        afterConnection();
+      });
+
+      function afterConnection() {
+        connection.query("SELECT * FROM employees", function(err, res) {
+          if (err) throw err;
+          console.log(res);
+          connection.end();
+        });
+      }
 }
-]
+contactServer();
 
-const tableToDisplay = cTable.getTable(testArray);
+// Used to display information to the console in a table format
+function displayInfo(){
+    const testArray = [
+        {name: 'foo',
+        type: 'test'
+    }, 
+        {name: 'bar',
+        type: 'test'
+    },
+        {name: 'jake',
+        type: 'test'
+    }
+    ]
 
-console.log(tableToDisplay);
+    const tableToDisplay = cTable.getTable(testArray);
+
+    console.log(tableToDisplay);
+}
+
+// function promptUser(){
+//     inquirer
+//         .prompt([
+//             /* Pass your questions in here */
+//         ])
+//         .then(answers => {
+//             // Use user feedback for... whatever!!
+//         });
+// }
--- seed.sql
+++ seed.sql
@@ -1,9 +1,13 @@
+-- Deletes database if it already exists
 DROP DATABASE IF EXISTS employee_tracker_db;
 
+-- Creates database called employee_tracker_db.
 CREATE DATABASE employee_tracker_db;
 
+-- Tells mySQL to use employee_tracker_db for the following code.
 USE employee_tracker_db;
 
+-- Creates table used for storing employees
 CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
@@ -13,6 +17,7 @@ CREATE TABLE employees (
   PRIMARY KEY (id)
 );
 
+-- Creates table used for storing roles
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NULL,
@@ -22,8 +27,13 @@ CREATE TABLE roles (
   PRIMARY KEY (id)
 );
 
+-- Creates table used for storing departments
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
+
+-- Using these as temp test values
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Jake", "O'Toole", 1, 1), ("Kristen", "O'Toole", 2, 2), ("Penny", "O'Toole", 3, 2);
