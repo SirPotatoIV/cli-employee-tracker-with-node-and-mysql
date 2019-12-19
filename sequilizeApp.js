1e3144d5d512c43cfcb656c932de045a6ca497c8
SirPotatoIV <otoolej33@gmail.com>
2019-12-18 21:19:00 -0600

    Added a main prompt to be the initial prompt to the user.

--- sequilizeApp.js
+++ sequilizeApp.js
@@ -8,14 +8,46 @@ const Employees = require("./models/employees.js");
 const Roles = require("./models/roles.js");
 const Departments = require("./models/departments.js");
 
+// object used to reference the models with strings.
 const tables = {
     Employees,
     Roles,
     Departments
 }
 
+async function mainPrompt(){
+    const view = "View data from a table"
+    const add = "Add data to a table"
+    const update = "Update Employee Role"
+    
+    const userSelection =  await inquirer
+        .prompt({
+            type: "list",
+            name: "option",
+            message: "What would you like to do?",
+            choices: [view, add, update]
+        });
+
+    switch(userSelection.option){
+        case view: 
+            viewData()
+            break
+        case add:
+            addData()
+            break
+        case update:
+            updateData()
+            break
+    }
+}
+mainPrompt();
+
 // View a table
-async function findData(){
+// -- First prompts user
+// -- Second queries data
+// -- Calls the displayData function
+async function viewData(){
+    // Asks user what table they want to view.
     const tableSelected =  await inquirer
         .prompt({
             type: "list",
@@ -30,12 +62,16 @@ async function findData(){
         displayData(foundData)
      
 }
-findData()
+// findData()
+
+async function addData(){
+
+}
+
 
 // Display data from the database to the console in a table format
 function displayData(dataToDisplay){
-    const table = cTable.getTable(dataToDisplay);
+    const formattedData = cTable.getTable(dataToDisplay);
           
-    console.log(table);
-    
+    console.log(formattedData);
 }
