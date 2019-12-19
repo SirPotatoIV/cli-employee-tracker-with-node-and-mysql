cfe74ad0d1bfd810d3408330447c9ffc664c5c7a
SirPotatoIV <otoolej33@gmail.com>
2019-12-18 22:16:25 -0600

    Completed add function. Prompts user for table to update, prompts user with corresponding values needed for that table, and updated comments. Started updateData function, but have not completed.

--- sequilizeApp.js
+++ sequilizeApp.js
@@ -43,12 +43,13 @@ async function mainPrompt(){
             return;
     }
 }
-// mainPrompt();
+mainPrompt();
 
 // View a table
-// -- First prompts user
-// -- Second queries data
-// -- Calls the displayData function
+// -- Prompts user to figure out what table they would like to view
+// -- Query the database for the requested table
+// -- Display the requested table using the displayData function
+// -- Call main prompt to cause recursion 
 async function viewData(){
     // Asks user what table they want to view.
     const tableSelected =  await inquirer
@@ -65,21 +66,97 @@ async function viewData(){
         displayData(foundData)
      
 }
-// findData()
 
+// Add data to a table
+// -- Prompt user to figure out what table they want to add data to
+// -- Prompt user with data required for requested table
+// -- Query the database to create a new row in the requested table with the provided data
+// -- Call main prompt to cause recursion
 async function addData(){
-    const result = await Departments.create({
-        title: "Regulatory"
-    })
-    console.log(result);
-}
-addData()
+    // Contains questions pertaining to each table
+    const tableQuestions = {
+        Employees: [
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the Employees First Name?"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is the Employees Last Name?" 
+            },
+            {
+                type: "input",
+                name: "role_id",
+                message: "What is the Employees Role Id?"
+            },
+            {
+                type: "input",
+                name: "manager_id",
+                message: "What is the Employees Manager Id?"
+            }
+        ],
+        Roles: [
+            {
+                type: "input",
+                name: "title",
+                message: "What is the title of the role?"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What is the salary for this role?",
+                filter: function(input){
+                    return parseInt(input)
+                }
+            },
+            {
+                type: "input",
+                name: "department_id",
+                message: "What is the department id for this role?",
+                filter: function(input){
+                    return parseInt(input)
+                }
+            }
+        ],
+        Departments: [
+            {
+                type: "input",
+                name: "title",
+                message: "What is the title of the department?"
+            }
+        ]
+    }
+    // Prompts the user to figure out what table they want to add data to
+    const tableSelected =  await inquirer
+        .prompt({
+            type: "list",
+            name: "option",
+            message: "What table would you like to add to?",
+            choices: ["Employees", "Roles", "Departments"]
+        })
+    // Requests values from the user to add to the selected table
+    const rowValues = await inquirer
+        .prompt(
+            tableQuestions[tableSelected.option]
+        )
+    // Queries the server to add requested data to the selected table
+    const result = await tables[tableSelected.option].create(rowValues)
+    // Calls main prompt to cause recursion
+    mainPrompt();
 
+}
+
+// Update employee role
+async function updateData(){
+
+
+}
 
 // Display data from the database to the console in a table format
 function displayData(dataToDisplay){
     const formattedData = cTable.getTable(dataToDisplay);
-          
-    console.log(formattedData);
+
     mainPrompt();
 }
